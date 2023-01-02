e65b5de9ab51f3af4b6eda9f4f4d144984622746
kavtaraG <g.kavtaradze84@gmail.com>
2023-01-02 20:17:47 +0400

    added some cheges and new features 02.01.2023

--- wps/public/javascripts/home.js
+++ wps/public/javascripts/home.js
@@ -6,8 +6,17 @@ function myFunction_movies() {
     window.location.href="/movies"
 }
 
+function myFunction_store() {
+    window.location.href="/store"
+}
+
+function myFunction_login() {
+    window.location.href="/login"
+}
 
 module.exports = {
     myFunction,
-    myFunction_movies
+    myFunction_movies,
+    myFunction_store,
+    myFunction_login
 }
--- wps/routes/index.js
+++ wps/routes/index.js
@@ -7,7 +7,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/movies', function(req, res, next) {
-  res.render('movies', { title: 'Express' });
+  console.log(req.query);
+  res.render('movies', { title: 'Movies', qs: req.query });
+});
+
+router.get('/store', function(req, res, next) {
+  console.log(req.query);
+  res.render('store', { title: 'Store', qa: req.query });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
